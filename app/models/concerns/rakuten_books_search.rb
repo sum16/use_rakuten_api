71e5b6672b66a0f2753f713c2f2607002243789e
yasuyuki sato <godasper@gmail.com>
2021-11-22 18:45:14 +0900

    トランザクションを記述

--- app/models/concerns/rakuten_books_search.rb
+++ app/models/concerns/rakuten_books_search.rb
@@ -11,21 +11,16 @@ module RakutenBooksSearch
     if @title.present?
       # resultsに楽天APIから取得したjsonデータを格納
       # 書籍のタイトルを検索して、一致するデータを格納するように設定
-      results = RakutenWebService::Books::Book.search({keyword: @title})
+      results = RakutenWebService::Books::Book.search({title: @title})
         
       # @booksに格納
       results.each do |result|
-        book = Book.new(read(result))
-        @books << book
+          book = Book.new(read(result))
+          @books << book
       end
     end
     
-    #すでに保存済の本は除外するためにunlessで切り分け
-    @books.each do |book|
-      unless Book.all.include?(book)
-        book.save
-      end
-    end
+    book_save
   end
 
   def read(result)
@@ -44,10 +39,28 @@ module RakutenBooksSearch
       image_url: image_url
     }
   end
+
+  def book_save
+    # すでに保存済の本はunlessで除外
+    # トランザクション内でrescueを使用するとロールバックが行われないため以下のようにトランザクションの外側で例外をキャッチする
+    Book.transaction do
+      @books.each do |book|
+        unless Book.all.include?(book)
+          book.save!
+         end
+      end
+    end
+    # 1
+    rescue ActiveRecord::Rollback, "ロールバックする"
+    Rails.logger.debug e.message
+  end
 end
 
-# gsubメソッドは特定の文字を別の文字へ置換するだけでなく、正規表現を用いて該当する箇所を置換したり削除できるメソッド
+# 1
+# 例外処理がメソッド定義全体に対して行われる場合には begin と end を省略できる
 
+
+# gsubメソッドは特定の文字を別の文字へ置換するだけでなく、正規表現を用いて該当する箇所を置換したり削除できるメソッド
 # 正規表現を使わない場合
 # 文字列.gsub(置換したい文字列, 置換後の文字列)
 
